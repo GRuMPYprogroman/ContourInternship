f5064279b7877e92b9ca835dea42e7d840f5292b
Степан Лазарев <grebdloz@mail.ru>
2026-04-26 21:44:17 +0300

    Implemented UI

---
+++ WpfApp/WpfApp/Attributes/FileExtensionAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace WpfApp.Attributes;
+
+public class FileExtensionAttribute(string extension) : ValidationAttribute
+{
+    public string Extension { get; } = extension.StartsWith(".") ? extension : "." + extension;
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        string path = value?.ToString() ?? string.Empty;
+
+        if (string.IsNullOrEmpty(path))
+            return new ValidationResult("Provide path to file");
+
+        if (!string.Equals(Path.GetExtension(path), Extension, StringComparison.OrdinalIgnoreCase))
+            return new ValidationResult($"File extension must be {Extension}");
+        
+        return ValidationResult.Success;
+    }
+}
---
+++ WpfApp/WpfApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using WpfApp.Attributes;
+
+namespace WpfApp.ViewModels;
+
+public partial class MainWindowViewModel : ObservableValidator
+{
+    [ObservableProperty]
+    [NotifyDataErrorInfo]
+    [NotifyCanExecuteChangedFor(nameof(StartProcessCommand))]
+    [FileExtension(".dat")]
+    private string? _datFilePath;
+    
+    [ObservableProperty]
+    [NotifyDataErrorInfo]
+    [NotifyCanExecuteChangedFor(nameof(StartProcessCommand))]
+    [FileExtension(".csv")]
+    private string? _csvFilePath;
+    
+    [RelayCommand]
+    private void OpenFile(string extension)
+    {
+        var dialog = new OpenFileDialog()
+        {
+            Title = "Open File",
+            DefaultExt = ".csv",
+            Filter = "CSV Files (.csv)|*.csv| DAT Files (*.dat)|*.dat",
+            Multiselect = false,
+        };
+        
+        bool? result = dialog.ShowDialog();
+        
+        if (result != true)
+            return;
+
+        switch (extension)
+        {
+            case ".csv":
+                CsvFilePath = dialog.FileName;
+                break;
+            case ".dat":
+                DatFilePath = dialog.FileName;
+                break;
+        }
+    }
+
+    [RelayCommand(CanExecute = nameof(CanStartProcess))]
+    private void StartProcess()
+    {
+        
+    }
+
+    private bool CanStartProcess => !HasErrors && !string.IsNullOrWhiteSpace(DatFilePath) 
+                                               && !string.IsNullOrWhiteSpace(CsvFilePath);
+}
